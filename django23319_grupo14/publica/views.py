cea710f414438c0efc0b0bdea9bd05152e48e509
Pablo Alustiza <alustiza.p@gmail.com>
2023-04-13 10:08:30 -0300

    update

--- django23319_grupo14/publica/views.py
+++ django23319_grupo14/publica/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from datetime import datetime
 
 # Create your views here.
 def hola_mundo(request):
@@ -23,8 +24,51 @@ def hola_mundo(request):
 # http://127.0.0.1:8000/?param=2024&param2=hola20202
 
 
+# def index(request):
+#     return render(request,'publica/index.html')
+
+
+# si quiero enviar un param por el navegador escribo en la url
+# http://127.0.0.1:8000/?param=lalaloopp&param2=lolo
+
+
 def index(request):
-    return render(request,'publica/index.html')
+    if(request.method=='GET'):
+        titulo = 'Titulo cuando accedo por GET'
+    else:
+        titulo = 'Titulo cuando accedo por otro metodo'
+    parametro_uno = request.GET.get('param')
+    parametro_dos = request.GET.get('param2')
+    listado_cursos = [
+        {
+            'nombre':'Fullstack Java',
+            'descripcion':'Curso de Fullstack',
+            'categoria':'Programación',
+        },
+        {
+            'nombre':'Diseño UX/UI',
+            'descripcion':'🖌🎨',
+            'categoria':'Diseño',
+        },
+        {
+            'nombre':'Big Data',
+            'descripcion':'test',
+            'categoria':'Análisis de Datos',
+        },
+        {
+            'nombre':'Habilidades blandas',
+            'descripcion':'accenture',
+            'categoria':'Análisis de Datos',
+        },
+    ]
+
+    context = {'titulo':titulo,
+                'parametro_uno':parametro_uno,
+                'hoy':datetime.now(),
+                'cursos':listado_cursos
+            }
+    
+    return render(request,'publica/index.html',context)
 
 
 def saludar(request, nombre):
